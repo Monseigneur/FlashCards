1ef8b456e33efc318328088eb49ebd3fcebe5197
Monseigneur <milan.justel@gmail.com>
2012-06-14 17:17:33 -0700

    edits

---
+++ src/util/FlashCard.java
@@ -0,0 +1,15 @@
+package util;
+
+public class FlashCard {
+	public String front;
+	public String back;
+	
+	public FlashCard(String firstWord, String secondWord) {
+		this.front = firstWord;
+		this.back = secondWord;	
+	}	
+	
+	public String toString() {
+		return "{" + front + " == " + back + "}";	
+	}
+}
